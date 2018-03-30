41a153720bb7c14b55ccbd7cfd76245ce0b06ed8
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-03-30 09:32:04 +0200

    Add login and signup pages

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+	if ( isset($_SESSION['email'])) {
+	}
+	else {
+		header('Location: login.php');
+	}
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Homepage</h1>
+</body>
+</html>
