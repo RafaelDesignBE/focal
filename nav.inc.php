a815915fb0692a86b440f1ac2c845b3a0ad6e96b
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-05-12 21:36:30 +0200

    Redesign optimized for mobile use

--- nav.inc.php
+++ nav.inc.php
@@ -1,6 +1,5 @@
 <?php
 
-  session_start();
 
 ?><nav class="navbar">
 
@@ -8,15 +7,17 @@
 
       <a class="link--h1" href="index.php"><h1 class="h1--focal">Focal</h1></a>
 
-      <div class="navbar__icons">
-        <a href="upload.php"><img class="navbar__icons--icon" src="public_html/img/upload.svg" alt="upload"></a>
-        <a href="#"><img class="navbar__icons--icon" src="public_html/img/notification.svg" alt="notification"></a>
-        <a href="profile.php?user=<?php echo $_SESSION['user_id'] ?>"><img class="navbar__icons--icon navbar__icons--rightIcon" src="public_html/img/profile.svg" alt="profile"></a>
-      </div>
-
       <form class="form__search" action="search.php" method="get">
         <input placeholder="Search" type="text" name="q">
       </form>
+
+      <div class="navbar__icons">
+        <a href="#"><img class="navbar__icons--icon" src="public_html/img/notification.svg" alt="notification"></a>
+        <a href="upload.php"><img class="navbar__icons--icon" src="public_html/img/upload.svg" alt="upload"></a>
+        <a href="profile.php?user=<?php echo $_SESSION['user_id'] ?>"><img class="navbar__icons--icon navbar__icons--rightIcon" src="public_html/img/profile.svg" alt="profile"></a>
+      </div>
+
+      
     
     <!--<a href="logout.php" class="btn btn--secondary">Log out</a>-->
     </div>
