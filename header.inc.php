2fed52dab67b263057ec0dbf326c5b3c28307089
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-05-14 17:54:04 +0200

    Add favicons, browserconfig and manifest

--- header.inc.php
+++ header.inc.php
@@ -1,6 +1,23 @@
 <link rel="stylesheet" href="./public_html/css/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cssgram/0.1.10/cssgram.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<link rel="apple-touch-icon" sizes="57x57" href="public_html/img/icons/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="public_html/img/icons/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="public_html/img/icons/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="public_html/img/icons/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="public_html/img/icons/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="public_html/img/icons/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="public_html/img/icons/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="public_html/img/icons/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="public_html/img/icons/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="public_html/img/icons/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="public_html/img/icons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="public_html/img/icons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="public_html/img/icons/favicon-16x16.png">
+<!--[if IE]><link rel="shortcut icon" href="public_html/img/icons/favicon.ico"/><![endif]-->
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="public_html/img/icons/ms-icon-144x144.png">
+<meta name="theme-color" content="#616EFF">
 <script>
 function maxSearch(){
     $('.h1--focal').css('width', '0');
