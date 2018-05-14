fea224d864180362610dce805e87e90b3b8d71d7
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-05-14 17:23:40 +0200

    Edit title for edit profile

--- editProfile.php
+++ editProfile.php
@@ -42,7 +42,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php include_once('header.inc.php'); ?>
-    <title>Home</title>
+    <title>Edit Profile</title>
 </head>
 <body>
     <?php include_once("nav.inc.php"); ?>
